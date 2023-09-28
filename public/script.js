c1e0703ac721a5dd32d5cc7420df6fd296746b0b
Dhillyon Barrett <ed20db@leeds.ac.uk>
2023-09-28 13:21:36 +0100

    Routing, form functionality, database initialisation

---
+++ public/script.js
@@ -0,0 +1,41 @@
+const labels = document.querySelectorAll('form label')
+
+labels.forEach(label => {
+    label.innerHTML = '<i class="fa-solid fa-arrow-right"></i>' + label.innerHTML
+})
+
+const addButton = document.getElementById('add')
+const buttons = document.getElementById('buttons')
+const form = document.querySelector('form')
+var labelCount = 2
+
+addButton.addEventListener('click', addNewOption)
+
+function addNewOption() {
+    labelCount ++
+    const newNode = document.createElement('label')
+    form.insertBefore(newNode, buttons)
+    document.querySelector('label:last-of-type').innerHTML = '<i class="fa-solid fa-arrow-right"></i>' + 
+    '<input type="text" required class="options" placeholder="Option ' + labelCount+ '"/>'
+}
+
+async function funcSubmit(e) {
+    const question = document.getElementById('question').value 
+    const options = Array.from(document.querySelectorAll('.options')).map(option => option.value)
+    
+    const idsRes = await fetch('/ids')
+    const {ids} = await idsRes.json()
+    const id = ids.length === 0 ? 1: Math.max(...ids) + 1
+
+    const result = await fetch('/', {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({
+            id,
+            question,
+            options
+        })
+    })
+}
--- server.js
+++ server.js
@@ -2,11 +2,50 @@ const express = require('express')
 const app = express()
 const port = 8383
 
+const {readDb, writeDb} = require('./dbFunctions')
+
 app.use(express.static('public'))
+app.use(express.json())
 
 // App routes
 app.post("/", (request, response) => {
-    response.send("wiajdoawjd")
+    const {id, question, options} = request.body
+    
+    if (!id || !question || options.length == 0) {
+        response.status(400).send({status:"error"})
+    }
+
+    console.log(id, question, options)
+
+    const currentPolls = readDb()
+    writeDb({
+        ...currentPolls,
+        [id]: {
+            question,
+            options: options.reduce((acc, curr) => {
+                return {...acc, [curr]: 0}
+            }, {})
+        }
+    })
+
+    response.sendStatus(200)
+})
+
+// Response is used to assert that any new ID is unique
+app.get('/ids', (request, response) => {
+    const ids = readDb()
+    response.status(200).send({ids: Object.keys(ids)})
+})
+
+app.get('/:id', (request, response) => {
+    const {id} = request.params
+    console.log(id)
+    try {
+        return response.status(200).sendFile('poll.html', {root: __dirname + '/public'})
+    } catch (err) {
+        console.error(err)
+        response.sendStatus(500)
+    }
 })
 
 app.listen(port, () => console.log('Server running on port ' + port))
